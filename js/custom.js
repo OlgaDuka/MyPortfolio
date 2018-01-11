0a9541a1cce0416998968ef13618a667f162f82b
Olga Duka <olgaduk@mail.ru>
2018-01-11 22:31:21 +0600

    Исправление ошибок, шлифовка

--- js/custom.js
+++ js/custom.js
@@ -18,7 +18,6 @@
 
     $('a[href*=#]').bind('click', function (e) {
       var anchor = $(this);
-      wow.init();
       $('html, body').stop().animate({scrollTop: $(anchor.attr('href')).offset().top}, 1000);
       e.preventDefault();
     });
@@ -30,14 +29,24 @@
         $('.scroll--up').fadeOut();
       }
     });
-    // Первый активный пункт меню после главной страницы
-    $('.scroll__down').bind('click', function () {
-      menuItems[1].classList.add('navbar__item--active');
-    });
 
     // ----------------------------------------------
     // Меню
     // ----------------------------------------------
+
+    // Сброс признака "активный" с пунктов меню
+    var menuDeactivate = function () {
+      [].forEach.call(menuItems, function (element) {
+        element.classList.remove('navbar__item--active');
+      });
+    };
+
+    // Первый активный пункт меню после главной страницы
+    $('.scroll__down').bind('click', function () {
+      menuDeactivate();
+      wow.init();
+      menuItems[1].classList.add('navbar__item--active');
+    });
 
     // Садим меню на стикер вверху экрана
     $('.header').sticky({topSpacing: 0});
@@ -52,12 +61,11 @@
 
     // Переключаем активный пункт меню и закрываем меню, если оно выпадающее
     var onMenuItemClick = function (evt) {
-      [].forEach.call(menuItems, function (element) {
-        element.classList.remove('navbar__item--active');
-      });
-      evt.target.classList.add('navbar__item--active');
+      menuDeactivate();
+      evt.currentTarget.classList.add('navbar__item--active');
       menu.classList.add('navbar__list--close');
     };
+
     // Добавляем событие клика на пункты меню
     [].forEach.call(menuItems, function (element) {
       element.addEventListener('click', onMenuItemClick);
@@ -67,10 +75,10 @@
     // Эффект параллакса на домашней странице
     // ----------------------------------------------
 
-    $('.screen-height').height($(window).height());
+    $('.home__screen-height').height($(window).height());
 
     $(window).resize(function () {
-      $('.screen-height').height($(window).height());
+      $('.home__screen-height').height($(window).height());
     });
 
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
